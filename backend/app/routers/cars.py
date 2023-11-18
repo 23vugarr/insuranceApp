1e2b28e8a828a7fddd14d9ba57924b2ff462a61e
Vugar Abdulali <vabdulaliyev@gmail.com>
2023-11-19 02:05:57 +0400

    commit

--- backend/app/routers/cars.py
+++ backend/app/routers/cars.py
@@ -4,8 +4,10 @@ from app.core.database import get_db
 from app.dependencies.authentication import oauth2_scheme
 from app.models.cars import CarDetails as CarDetailsModel
 from fastapi.responses import StreamingResponse
-import io
+from datetime import date
+import base64
 import requests
+import codecs
 
 
 
@@ -56,14 +58,48 @@ async def company(token: dict = Depends(oauth2_scheme),
     
 #     return StreamingResponse(io.BytesIO(file_content), media_type='image/jpeg')
 
+
 @router.post("/report/{car_id}", dependencies=[Depends(oauth2_scheme)])
 async def report(file: UploadFile,
                  car_id: int,
                  token: dict = Depends(oauth2_scheme),
                  db: Session = Depends(get_db)):
+    
     file_content = await file.read()
+    base64_bytes = base64.b64encode(file_content)
+    base64_string = "data:image/jpeg;base64,"+base64_bytes.decode()
+
+    response = requests.post("https://23vugarr-car-parts-damage-detection.hf.space/run/predict", json={
+          "data": [
+            base64_string,
+        ]
+    }).json()
     
-    result_as_list = list(file_content)
-    print(result_as_list)
+    damages = response["data"][0][21:]
+    damages = base64.b64decode(damages)
+    damages = list(damages)
+
+    scratches = response["data"][1]
+    scratches = base64.b64decode(scratches)
+    scratches = list(scratches)
+
+    car_parts = response["data"][2]
+    car_parts = base64.b64decode(car_parts)
+    car_parts = list(car_parts)
+
+    damages_info_str = response["data"][3]
+
+    overlayed_damage = response["data"][4]
+    overlayed_damage = base64.b64decode(overlayed_damage)
+    overlayed_damage = list(overlayed_damage)
     
-    return result_as_list
+    return {
+        "result": {
+            "damages": damages,
+            "scratches": scratches,
+            "car_parts": car_parts,
+            "damages_info_str": damages_info_str,
+            "overlayed_damage": overlayed_damage,
+            "date": date.today(),
+        }
+    }
